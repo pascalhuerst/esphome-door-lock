5a1d7705586f39e7b457e318bb106f7e9320b987
Pascal Huerst <pascal.huerst@gmail.com>
2021-03-26 18:39:35 +0100

    Adapt spi pins to my setup

--- door_lock_rfid.h
+++ door_lock_rfid.h
@@ -196,6 +196,7 @@ class RfidSensorsComponent : public Component {
   void setup() override {
     SPIFFS.begin();
     SPI.begin();
+    SPI.pins(D5, D6, D7, D0);
     rfid.PCD_Init(D8, D3);
 
     load_users_list();
